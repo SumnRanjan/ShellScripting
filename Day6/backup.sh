4b7d2a756238cc338b273a36b96063ddb52b93e6
Suman Ranjan <sumanranjan17291729@gmail.com>
2026-03-30 12:08:23 +0530

    Add Makefile and backup script with backup rotation logic; create initial data files and log

---
+++ Day6/backup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+<< readme 
+This is a Script for backup with 5 day rotation
+Useage:
+./backup.sh <path to yout source> <path to backup folder>
+readme
+
+function display_usage {
+	
+	echo "Usage : ./backup.sh <path to your source> <path to backup folder>"
+}
+
+if [ $# -eq 0 ];then
+	display_usage
+fi
+
+source_dir=$1
+timestamp=$(date '+%Y-%m-%d-%H-%M-%S')
+backup_dir=$2
+
+
+function create_backup {
+	zip -r  "${backup_dir}/backup_${timestamp}.zip" "${source_dir}" > /dev/null
+	if [ $? -eq 0 ]; then
+	echo "backup generated successfully for ${timestamp}"  
+	fi
+}
+
+function perform_rotation {
+	backups=($(ls -t "${backup_dir}/backup_"*.zip 2>/dev/null)) 
+	
+	if [ "${#backups[@]}" -ge 5 ];then
+		echo "Performing rotation for 5 days"
+		
+		backup_to_remove=("${backups[@]:5}")
+		#echo "${backup_to_remove[@]}"
+ 		
+		for backup in "${backup_to_remove[@]}";
+		do
+			rm -f ${backup}
+		done  
+	fi 
+}
+create_backup
+perform_rotation
+
